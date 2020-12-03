4ac2964384fa13ae84e16b27cd13a6e79f035475
olgasem10 <olga.sem10@gmail.com>
2020-12-03 14:06:37 +0300

    Continuation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 //$(function() {
-//    $('#home-tab').css("background-color", "#7ecbc6")
+//    $('#markup_bt').css("background-color", "#7ecbc6");
 //})
+
+//$(function() {
+//    $('.descriptor').click(function (){
+//        alert('its working!');
+//    });
+//})
+
+
+
+$(function() {
+    $('#markup_bt').on('click', function() {
+        $(".try").removeAttr("hidden");
+        //alert('its working!');
+    });
+})
